c12401ef5353d926d845928beaa560d4fff996af
Rimnao <rom123maurice@gmail.com>
2024-12-05 19:14:12 +0100

    remise a jour de la branche dev

--- main.py
+++ main.py
@@ -1,98 +1,67 @@
-from cryptography.fernet import Fernet
+import DBInterraction
 
+currentDictionaryId = False
 
-class PasswordManager:
+DioctionaryConnexion = False
 
-    def __init__(self):
-        self.key = None
-        self.password_file = None
-        self.password_dict = {}
-
-    def create_key(self, path):
-        self.key = Fernet.generate_key()
-        with open(path, 'wb') as f:
-            f.write(self.key)
-
-    def load_key(self, path):
-        with open(path, 'rb') as f:
-            self.key = f.read()
-
-    def create_pasword_file(self, path, initial_values=None):
-        self.password_file = path
-
-        if initial_values is not None:
-            for key, value in initial_values.items():
-                self.add_password(key, value)
-
-    def load_password_file(self, path):
-        self.password_file = path
-
-        with open(path, 'r') as f:
-            for line in f:
-                site, encrypted = line.split(":")
-                self.password_dict[site] = Fernet(self.key).decrypt(encrypted.encode()).decode()
-
-
-    def add_password(self, site, password):
-        self.password_dict[site] = password
-
-        if self.password_file is not None:
-            with open(self.password_file, 'a+') as f:
-                encrypted = Fernet(self.key).encrypt(password.encode())
-                f.write(site + ":" + encrypted.decode() + "\n")
-
-    def get_password(self, site):
-        return self.password_dict[site]
-
-def main():
-    # password = {
-    #     "email": "1234567",
-    #     "facebook": "myfbpassword",
-    #     "youtube": "helloworld123",
-    #     "something": "myfavoritepassword_123"
-    # }
-
-    pm = PasswordManager()
+while DioctionaryConnexion == False: #connexion au coffre fort
 
     print("""What do you want to do ?
-          (1) Create a key
-          (2) Load an existing key
-          (3) Create new password file
-          (4) Load existing password file
-          (5) Add a new password
-          (6) Get a password
-          (q) Quit
-          """)
-    
-    done = False
+      (1) Créer un coffre fort
+      (2) Charger un coffre fort
+      (q) Quit
+      """)
+        
+    choice = input("enter your choice: ")
+    if choice == "1":
+        pw = input("Entrez le mot de passe du coffre : ")
+        pw2 = input("Entrez le mot de passe une seconde fois : ")
+        if pw != pw2:
+            print("les mots de passes sont différents")
+            exit
+        else:
+            currentDictionaryId = DBInterraction.create_dictionary(pw)
+    elif choice == "2":
+        pw = input("Entrez le mot de passe du coffre : ")
+        currentDictionaryId = DBInterraction.connection_dictionary(pw)
+    elif choice == "q":
+        DioctionaryConnexion = True
+        print ("Bye")
+    else :
+        print("Invalid choice!")
 
-    while not done:
+    if currentDictionaryId != False :
+        DioctionaryConnexion = True
 
-        choice = input("enter your choice: ")
-        if choice == "1":
-            path = input("Enter path: ")
-            pm.create_key(path)
-        elif choice == "2":
-            path = input("Enter path: ")
-            pm.load_key(path)
-        elif choice == "3":
-            path = input("Enter path: ")
-            pm.create_pasword_file(path)
-        elif choice == "4":
-            path = input("Enter path: ")
-            pm.load_password_file(path)
-        elif choice == "5":
-            site = input("Enter the site: ")
-            password = input("Enter the password: ")
-            pm.add_password(site, password)
-        elif choice == "6":
-            site = input("What site do you want: ")
-            print(f"password for {site} is {pm.get_password(site)}")
-        elif choice == "q":
-            done = True
-            print ("Bye")
-        else :
-            print("Invalid choice!")
+    print("connexion au coffre fort")
+        
 
-if __name__ == "__main__":
-    main()
+Actions = False
+
+while Actions == False: #action dans le coffre fort
+
+    print("""What do you want to do ?
+    (1) Voir mes mots de passe
+    (2) Ajouter un mot de passe
+    (3) Supprimer un mot de passe
+    (4) Editer un mot de passe
+    (5) Exporter votre coffre fort
+    (q) Quit
+    """)
+
+    choice = input("enter your choice: ")
+    if choice == "1":
+        a
+    elif choice == "2":
+        a
+    elif choice == "3":
+        a
+    elif choice == "4":
+        a
+    elif choice == "5":
+        print("ouais exportation")
+    elif choice == "q":
+        Actions = True
+        print ("Bye")
+    else :
+        print("Invalid choice!")
