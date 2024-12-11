58f986bcb75a80d3b44678803e1425f4a2fb0e40
Vince-lgd <lagardevincent64@gmail.com>
2024-12-11 22:03:37 +0100

    merge dev

--- DBInterraction.py
+++ DBInterraction.py
@@ -1,13 +1,9 @@
 from cryptography.fernet import Fernet
-<<<<<<< HEAD
-import bcrypt
-=======
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.hashes import SHA256
 
 import bcrypt
 import base64
->>>>>>> dev
 
 import Database
 
